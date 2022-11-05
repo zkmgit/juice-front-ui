bb180207ac6f47a817f446a7699dfa22270a2b96
zkmian3364 <zkmian@3364>
2022-11-05 10:35:10 +0800

    封装统一的表格分页组件

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/api/login',
+    url: '/api/user/login',
     method: 'post',
     data
   })
--- src/mallManager/api/userManager/user.js
+++ src/mallManager/api/userManager/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 // 获取所有的用户
 export function getTableData(data) {
   return request({
-    url: '/api/getAllUserList',
+    url: '/api/user/getAllUserList',
     method: 'post',
     data
   })
