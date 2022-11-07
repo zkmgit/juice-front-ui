34d2a4c62a55ca8d1435c74b6a58065455effedc
zkmian3364 <zkmian@3364>
2022-11-07 12:18:24 +0800

    删除多余的文件

--- src/mallManager/api/userManager/user.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-// 获取所有的用户
-export function getTableData(data) {
-  return request({
-    url: '/api/user/getAllUserList',
-    method: 'post',
-    data
-  })
-}
